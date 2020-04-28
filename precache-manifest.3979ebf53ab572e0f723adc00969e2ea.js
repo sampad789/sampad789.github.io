b51750d9504cda1059a5539f71aae2e6db2cfe74
sampad789 <sampad789@gmail.com>
2020-04-28 13:06:54 +0300

    Changes made

---
+++ precache-manifest.3979ebf53ab572e0f723adc00969e2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d66ac5a7ddcfbd619ca8275c9780a23",
+    "url": "/static/media/me.6d66ac5a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c350f6126656cd0240c",
+    "url": "/static/js/main.8c350f61.chunk.js"
+  },
+  {
+    "revision": "adb1da8dd819054ebdf6",
+    "url": "/static/js/1.adb1da8d.chunk.js"
+  },
+  {
+    "revision": "8c350f6126656cd0240c",
+    "url": "/static/css/main.85057aa3.chunk.css"
+  },
+  {
+    "revision": "be006d825bba3e38e57e8f6cf7aebfc0",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.72286ff2bff8d6f706d98387f30476b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d66ac5a7ddcfbd619ca8275c9780a23",
+    "url": "/static/media/me.6d66ac5a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "663342fcb86256b92ad7",
+    "url": "/static/js/main.663342fc.chunk.js"
+  },
+  {
+    "revision": "088ec7c3a242cf897a1c",
+    "url": "/static/js/1.088ec7c3.chunk.js"
+  },
+  {
+    "revision": "663342fcb86256b92ad7",
+    "url": "/static/css/main.01183a84.chunk.css"
+  },
+  {
+    "revision": "79dbfa3ecf4a295c1678790970748693",
+    "url": "/index.html"
+  }
+];
